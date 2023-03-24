252626680f2d4e1ab274118e10e7e7b520b2468d
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-03-24 17:00:14 +0000

    Contain method done!

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Atom carbon = new Atom("Cl");
-        System.out.println(carbon.getBonds());
+        Atom carbon1 = new Atom("C");
+        Atom carbon2 = new Atom("C");
+        TreeMolecule treeMolecule = new TreeMolecule(carbon1);
+        System.out.println(treeMolecule.contains(carbon1));
     }
 }
--- TreeMolecule.java
+++ TreeMolecule.java
@@ -3,41 +3,60 @@
 
 //add import statement
 
-
-
 public class TreeMolecule implements Molecule {
 
-	private Atom first;
+        private Atom first;
 
-	
-        //add constructor, implemented methods and any helper methods you require
+        // add constructor, implemented methods and any helper methods you require
         public TreeMolecule(Atom atom) {
+                if (this.first != null) {
+                        throw new IllegalStateException("TreeMolecule already has a root atom");
+                }
                 this.first = atom;
         }
 
         @Override
         public boolean addBond(Atom a1, Atom a2, int strength) {
-            // TODO Auto-generated method stub
-            return false;
+                if (!this.contains(a1) || !this.contains(a2)) {
+                        return false;
+                }
+
+                if (a1.equals(a2)) {
+                        return false;
+                }
+                return false;
         }
 
         @Override
         public boolean contains(Atom a) {
-            // TODO Auto-generated method stub
-            return false;
+                return this.search(this.first, a);
+        }
+
+        private boolean search(Atom current, Atom target) {
+                if (current == null) {
+                        return false;
+                }
+                if (current.equals(target)) {
+                        return true;
+                }
+                for (Bond bond : current.getBonds()) {
+                        if (this.search(bond.getChild(), target)) {
+                                return true;
+                        }
+                }
+                return false;
         }
 
         @Override
         public String smilesString() {
-            // TODO Auto-generated method stub
-            return null;
+                // TODO Auto-generated method stub
+                return null;
         }
 
         @Override
         public String structuralFormula() {
-            // TODO Auto-generated method stub
-            return null;
+                // TODO Auto-generated method stub
+                return null;
         }
-	
-	
+
 }
